f8948c27e3300e1b0d0af9f07fb03b75e2685ce0
Daler Asrorov <asrorids@gmail.com>
2015-10-15 07:34:46 -0700

    Added file action.

--- proteus/frontend/js/main.js
+++ proteus/frontend/js/main.js
@@ -1,1 +1,12 @@
-//JavaScript code will be here...
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong>','</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
