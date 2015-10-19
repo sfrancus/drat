bb0c41151b99b5f64bebeab05e8dd22491d251f7
Daler Asrorov <asrorids@gmail.com>
2015-10-18 21:08:16 -0700

    Angular setup made

--- proteus/frontend/js/main.js
+++ proteus/frontend/js/main.js
@@ -1,12 +1,8 @@
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+angular
+  .module('drat', [])
+  .controller('switch', ['$scope', function($scope){
+      $scope.goToTwo = function() {
+        console.log("goes");
+      }
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong>','</li>');
-    }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-  }
-
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  }])
