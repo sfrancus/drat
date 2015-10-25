ae6244ccfb0807c5e171724ea99e11220fc991fa
Daler Asrorov <asrorids@gmail.com>
2015-10-24 21:47:13 -0700

    Progress bar added.

--- proteus/frontend/js/main.js
+++ proteus/frontend/js/main.js
@@ -1,8 +1,16 @@
 angular
-  .module('drat', [])
+  .module('drat', ['ngAnimate', 'ui.bootstrap'])
   .controller('switch', ['$scope', function($scope){
       $scope.goToTwo = function() {
         console.log("goes");
       }
+      $scope.max = 100;
+
+      $scope.generateProgress = function() {
+        var value = 80;
+
+        $scope.dynamic = value;
+      }
+      $scope.generateProgress();
 
   }])
